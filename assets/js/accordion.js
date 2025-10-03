00b525c13d7942388d92a3ec234cc72bde82b470
Chanwoo Kim <138036632+chanwoo-kim04@users.noreply.github.com>
2025-10-04 01:27:06 +0900

    Update accordion.js

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -2,12 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
   var headers = document.querySelectorAll(".accordion-header");
 
   headers.forEach(function(header) {
-    // Initially hide all panels
+    // Hide all panels by default
     var panel = header.nextElementSibling;
     panel.style.display = "none";
 
     header.addEventListener("click", function() {
+      // Toggle the 'active' class on the button
       this.classList.toggle("active");
+
+      // Toggle the panel display
       var panel = this.nextElementSibling;
       if (panel.style.display === "block") {
         panel.style.display = "none";
